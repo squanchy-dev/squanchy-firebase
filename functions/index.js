eb80da0be07c75d0bc117b877a95fcb1f2d0d380
Daniele Conti <daniele.conti@novoda.com>
2017-10-20 19:07:41 +0100

    Lol fml js

--- functions/index.js
+++ functions/index.js
@@ -19,46 +19,79 @@ exports.migrateToFirestore = functions.https.onRequest((request, response) => {
     const user_profiles = firestore.collection('user_profiles')
 
     const addAllDays = data => {
-        console.log("Add all days", data)
+        console.log('Add all days')
         return Promise.all(data.days.days.map(_day => {
             return days.add(day(new Date(_day.date), _day.position))
-                .then(docRef => (Object.assign({}, _day, { docRef })))
-        })).then(_days => (Object.assign({}, data, { days: { days: _days } })))
+                .then(docRef => Object.assign({}, _day, { docRef }))
+        })).then(_days => Object.assign({}, data, { days: { days: _days } }))
     }
 
     const addAllSpeakers = data => {
-        console.log("Add all speakers", data)
+        console.log('Add all speakers')
         return Promise.all(data.speakers.speakers.map(_speaker => {
-            return user_profiles.add(user_profile('', _speaker.name, _speaker.photo_url, _speaker.id))
+            return user_profiles.add(user_profile(null, _speaker.name, _speaker.photo_url, _speaker.id))
                 .then(userRef => speakers.add(speaker(
-                    '',
+                    null,
                     _speaker.bio,
                     _speaker.company_name,
                     _speaker.company_url,
-                    '',
+                    null,
                     _speaker.personal_url,
-                    '',
+                    null,
                     _speaker.twitter_username,
                     userRef
                 )))
-                .then(docRef => (Object.assign({}, _speaker, { docRef })))
-        })).then(_speakers => (Object.assign({}, data, { speakers: { speakers: _speakers } })))
+                .then(docRef => Object.assign({}, _speaker, { docRef }))
+        })).then(_speakers => Object.assign({}, data, { speakers: { speakers: _speakers } }))
     }
 
-    // const addAllEvents = data => {
-    //     return Promise.all(data.events.events.map(_event => {
-    //         return submissions.add(submission(
-    //             _event.description,
-    //             designCategory.path,
-    //             levelFor(_event.level),
-    //             '',
-    //             false,
-    //         ))
-    //     }))
-    // }
+    const addAllPlaces = data => {
+        console.log('Add all places')
+        return Promise.all(data.places.places.map(_place => {
+            return places.add(place(_place.name, _place.floor))
+                .then(docRef => Object.assign({}, _place, { docRef }))
+        })).then(_places => Object.assign({}, data, { places: { places: _places } }))
+    }
+
+    const addAllTracks = data => {
+        console.log('Add all tracks')
+        return Promise.all(data.tracks.tracks.map(_track => {
+            return tracks.add(track(_track.accent_color, _track.icon_url, _track.name, _track.text_color))
+                .then(docRef => Object.assign({}, _track, { docRef }))
+        })).then(_tracks => Object.assign({}, data, { tracks: { tracks: _tracks } }))
+    }
+
+    const addAllEvents = data => {
+        console.log('Add all events')
+        const _events = data.events.events
+        return Promise.all(Object.keys(_events).map(eventKey => _events[eventKey]).map(_event => {
+            return submissions.add(submission(
+                _event.description,
+                designCategory.path,
+                levelFor(_event.experience_level),
+                null,
+                false,
+                speakerRefsFor(data.speakers.speakers, _event.speaker_ids),
+                [],
+                _event.name,
+                null
+            )).then(submissionRef => events.add(event(
+                dayRefFor(data.days.days, _event.day_id),
+                new Date(_event.start_time),
+                new Date(_event.end_time),
+                placeRefFor(data.places.places, _event.place_id),
+                submissionRef,
+                trackRefFor(data.tracks.tracks, _event.track_id),
+                _event.type
+            )))
+        }))
+    }
 
     const levelFor = (level) => {
         switch (level) {
+            case undefined:
+            case null:
+                return null;
             case 'beginner':
                 return beginner
             case 'intermediate':
@@ -70,15 +103,26 @@ exports.migrateToFirestore = functions.https.onRequest((request, response) => {
         }
     }
 
+    const speakerRefsFor = (speakers, speakerIds) => speakers.filter(speaker => {
+        ids = speakerIds || []
+        ids.includes(speaker.id)
+    }).map(speaker => speaker.docRef)
+
+    const dayRefFor = (days, dayId) => days.find(day => day.id == dayId).docRef
+    const placeRefFor = (places, placeId) => places.find(place => place.id == placeId).docRef
+    const trackRefFor = (tracks, trackId) => tracks.find(track => track.id == trackId).docRef
+
     return firebaseApp.database().ref('data').once('value').then(snapshot => {
         return Promise.resolve(snapshot.val())
             .then(data => addAllDays(data))
+            .then(data => addAllPlaces(data))
+            .then(data => addAllTracks(data))
             .then(data => addAllSpeakers(data))
-        // .then(data => addAllEvents(data))
+            .then(data => addAllEvents(data))
     }).then(() => {
-        response.status(200).sent('Migration completed!')
+        response.status(200).send('Migration completed!')
     }).catch(error => {
-        console.error("Failed to migrate data", error)
+        console.error('Failed to migrate data', error)
         response.status(500).send()
     })
 });
