4e9b3232f133eb4c53ae8c578f83ad749d2c5372
Daniele Conti <daniele.conti@novoda.com>
2017-10-24 16:37:27 +0100

    Add explicit any types to migrate to firestore
    Yes this is crap but this function should be run only _once_ and was already working

--- functions/src/migrateToFirestore.ts
+++ functions/src/migrateToFirestore.ts
@@ -1,5 +1,5 @@
 import firebaseAdmin = require('firebase-admin')
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 import firebaseApp = firebaseAdmin.app.App
 
 export const migrateToFirestore = (firebaseApp: firebaseApp) => (request: Request, response: Response) => {
@@ -17,17 +17,17 @@ export const migrateToFirestore = (firebaseApp: firebaseApp) => (request: Reques
     const tracks = firestore.collection('tracks')
     const user_profiles = firestore.collection('user_profiles')
 
-    const addAllDays = data => {
+    const addAllDays = (data: any) => {
         console.log('Add all days')
-        return Promise.all(data.days.days.map(_day => {
+        return Promise.all(data.days.days.map((_day: any) => {
             return days.add(day(new Date(_day.date), _day.position))
-                .then(docRef => ({..._day, docRef }))
-        })).then(_days => ({...data, days: { days: _days } }))
+                .then(docRef => ({ ..._day, docRef }))
+        })).then(_days => ({ ...data, days: { days: _days } }))
     }
 
-    const addAllSpeakers = data => {
+    const addAllSpeakers = (data: any) => {
         console.log('Add all speakers')
-        return Promise.all(data.speakers.speakers.map(_speaker => {
+        return Promise.all(data.speakers.speakers.map((_speaker: any) => {
             return user_profiles.add(user_profile(null, _speaker.name, _speaker.photo_url, _speaker.id))
                 .then(userRef => speakers.add(speaker(
                     null,
@@ -40,27 +40,27 @@ export const migrateToFirestore = (firebaseApp: firebaseApp) => (request: Reques
                     _speaker.twitter_username,
                     userRef
                 )))
-                .then(docRef => ({..._speaker, docRef }))
-        })).then(_speakers => ({...data, speakers: { speakers: _speakers } }))
+                .then(docRef => ({ ..._speaker, docRef }))
+        })).then(_speakers => ({ ...data, speakers: { speakers: _speakers } }))
     }
 
-    const addAllPlaces = data => {
+    const addAllPlaces = (data: any) => {
         console.log('Add all places')
-        return Promise.all(data.places.places.map(_place => {
+        return Promise.all(data.places.places.map((_place: any) => {
             return places.add(place(_place.name, _place.floor))
-                .then(docRef => ({..._place, docRef }))
-        })).then(_places => ({...data, places: { places: _places } }))
+                .then(docRef => ({ ..._place, docRef }))
+        })).then(_places => ({ ...data, places: { places: _places } }))
     }
 
-    const addAllTracks = data => {
+    const addAllTracks = (data: any) => {
         console.log('Add all tracks')
-        return Promise.all(data.tracks.tracks.map(_track => {
+        return Promise.all(data.tracks.tracks.map((_track: any) => {
             return tracks.add(track(_track.accent_color, _track.icon_url, _track.name, _track.text_color))
-                .then(docRef => ({..._track, docRef }))
-        })).then(_tracks => ({...data, tracks: { tracks: _tracks } }))
+                .then(docRef => ({ ..._track, docRef }))
+        })).then(_tracks => ({ ...data, tracks: { tracks: _tracks } }))
     }
 
-    const addAllEvents = data => {
+    const addAllEvents = (data: any) => {
         console.log('Add all events')
         const _events = data.events.events
         return Promise.all(Object.keys(_events).map(eventKey => _events[eventKey]).map(_event => {
@@ -86,7 +86,7 @@ export const migrateToFirestore = (firebaseApp: firebaseApp) => (request: Reques
         }))
     }
 
-    const levelFor = (level) => {
+    const levelFor = (level: any) => {
         switch (level) {
             case undefined:
             case null:
@@ -102,14 +102,14 @@ export const migrateToFirestore = (firebaseApp: firebaseApp) => (request: Reques
         }
     }
 
-    const speakerRefsFor = (speakers, speakerIds) => speakers.filter(speaker => {
+    const speakerRefsFor = (speakers: any, speakerIds: any) => speakers.filter((speaker: any) => {
         const ids = speakerIds || []
         ids.includes(speaker.id)
-    }).map(speaker => speaker.docRef)
+    }).map((speaker: any) => speaker.docRef)
 
-    const dayRefFor = (days, dayId) => days.find(day => day.id == dayId).docRef
-    const placeRefFor = (places, placeId) => places.find(place => place.id == placeId).docRef
-    const trackRefFor = (tracks, trackId) => tracks.find(track => track.id == trackId).docRef
+    const dayRefFor = (days: any, dayId: any) => days.find((day: any) => day.id == dayId).docRef
+    const placeRefFor = (places: any, placeId: any) => places.find((place: any) => place.id == placeId).docRef
+    const trackRefFor = (tracks: any, trackId: any) => tracks.find((track: any) => track.id == trackId).docRef
 
     return firebaseApp.database().ref('data').once('value').then(snapshot => {
         return Promise.resolve(snapshot.val())
@@ -126,32 +126,45 @@ export const migrateToFirestore = (firebaseApp: firebaseApp) => (request: Reques
     })
 }
 
-const orNull = val => val || null
+const orNull = (val: any) => val || null
 
-const category = (name) => ({
+const category = (name: any) => ({
     name: orNull(name)
 })
-const day = (date, position) => ({
+const day = (date: any, position: any) => ({
     date: orNull(date),
     position: orNull(position)
 })
-const event = (day, start_time, end_time, place, submission, track, type) => ({
-    day: orNull(day),
-    start_time: orNull(start_time),
-    end_time: orNull(end_time),
-    place: orNull(place),
-    submission: orNull(submission),
-    track: orNull(track),
-    type: orNull(type)
-})
-const level = (name) => ({
+const event = (
+    day: any,
+    start_time: any,
+    end_time: any, place: any, submission: any, track: any, type: any) => ({
+        day: orNull(day),
+        start_time: orNull(start_time),
+        end_time: orNull(end_time),
+        place: orNull(place),
+        submission: orNull(submission),
+        track: orNull(track),
+        type: orNull(type)
+    })
+const level = (name: any) => ({
     name: orNull(name)
 })
-const place = (name, floor) => ({
+const place = (name: any, floor: any) => ({
     name: orNull(name),
     floor: orNull(floor)
 })
-const speaker = (address, bio, company_name, company_url, job_description, personal_url, phone_number, twitter_handle, user_profile) => ({
+const speaker = (
+    address: any,
+    bio: any,
+    company_name: any,
+    company_url: any,
+    job_description: any,
+    personal_url: any,
+    phone_number: any,
+    twitter_handle: any,
+    user_profile: any
+) => ({
     address: orNull(address),
     bio: orNull(bio),
     company_name: orNull(company_name),
@@ -162,7 +175,17 @@ const speaker = (address, bio, company_name, company_url, job_description, perso
     twitter_handle: orNull(twitter_handle),
     user_profile: orNull(user_profile)
 })
-const submission = (abstract, category, level, notes, isPrivate, speakers, tags, title, type) => ({
+const submission = (
+    abstract: any,
+    category: any,
+    level: any,
+    notes: any,
+    isPrivate: any,
+    speakers: any,
+    tags: any,
+    title: any,
+    type: any
+) => ({
     abstract: orNull(abstract),
     category: orNull(category),
     level: orNull(level),
@@ -173,13 +196,13 @@ const submission = (abstract, category, level, notes, isPrivate, speakers, tags,
     title: orNull(title),
     type: orNull(type)
 })
-const track = (accent_color, icon_url, name, text_color) => ({
+const track = (accent_color: any, icon_url: any, name: any, text_color: any) => ({
     accent_color: orNull(accent_color),
     icon_url: orNull(icon_url),
     name: orNull(name),
     text_color: orNull(text_color)
 })
-const user_profile = (email, full_name, profile_pic, user_id) => ({
+const user_profile = (email: any, full_name: any, profile_pic: any, user_id: any) => ({
     email: orNull(email),
     full_name: orNull(full_name),
     profile_pic: orNull(profile_pic),
