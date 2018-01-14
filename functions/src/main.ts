e7b43adec6f6c6b50dd2fb654f1bef6edd265a04
Daniele Conti <dnlcnt@gmail.com>
2018-01-14 00:14:13 +0000

    Remove unused import

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,4 +1,4 @@
-import { https, firestore, config } from 'firebase-functions'
+import { https, config } from 'firebase-functions'
 import { initializeApp } from 'firebase-admin'
 import { migrateToFirestore } from './migrateToFirestore'
 import { generateSchedule } from './schedule-view/generate-schedule'
