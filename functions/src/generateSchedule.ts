3c3f8b2795741fab3a445427b380a6e3f2a8e228
Daniele Conti <dnlcnt@gmail.com>
2018-01-14 00:05:21 +0000

    Add generate schedule function

---
+++ functions/src/generateSchedule.ts
@@ -0,0 +1,90 @@
+import { FirebaseApp } from "./firebase";
+import { Request, Response } from 'express'
+import {Speaker, SchedulePage } from "./scheduleData";
+import { DayData, EventData, SubmissionData, PlaceData, TrackData, SpeakerData, UserData, LevelData } from "./data";
+import { collection as firestoreCollection} from './firestore/collection'
+
+export const generateSchedule = (firebaseApp: FirebaseApp) => (request: Request, response: Response) => {
+    const firestore = firebaseApp.firestore();
+    const collection = firestoreCollection(firebaseApp)
+
+    const days = collection<DayData>('days')
+    const events = collection<EventData>('events')
+    const submissions = collection<SubmissionData>('submissions')
+    const places = collection<PlaceData>('places')
+    const tracks = collection<TrackData>('tracks')
+    const speakers = collection<SpeakerData>('speakers')
+    const users = collection<UserData>('user_profiles')
+    const levels = collection<LevelData>('levels')
+
+    Promise.all([
+        days,
+        events,
+        submissions,
+        places,
+        tracks,
+        speakers,
+        users,
+        levels
+    ]).then(([
+        days,
+        events,
+        submissions,
+        places,
+        tracks,
+        speakers,
+        users,
+        levels
+    ]) => {
+        const flattenedSpeakers = speakers.map(speaker => ({
+            speaker,
+            user: users.find(({id}) => speaker.user_profile.id === id)!
+        })).map(({speaker, user}): Speaker => ({
+            id: speaker.id,
+            name: user.full_name,
+            bio: speaker.bio,
+            companyName: speaker.company_name,
+            companyUrl: speaker.company_url,
+            twitterUsername: speaker.twitter_handle,
+            photoUrl: user.profile_pic,
+            personalUrl: speaker.personal_url
+        }))
+        const schedulePages = firestore.collection('views')
+            .doc('schedule')
+            .collection('schedule_pages')
+        return Promise.all(days.map(day => {
+            const schedulePage: SchedulePage = {
+                day,
+                events: events.map(event => {
+                    const submission = submissions.find(({ id }) => event.submission.id === id)!
+                    const place = places.find(({ id }) => event.place.id === id) || null
+                    const track = tracks.find(({ id }) => event.track.id === id) || null
+                    const submissionLevel = submission.level;
+
+                    const level = submissionLevel
+                        ? levels.find(({ id }) => submissionLevel.id === id)!.name
+                        : null
+
+                    const eventSpeakers = flattenedSpeakers.filter(({id}) => (event.speakers || []).findIndex(({id: speakerId}) => speakerId === id) !== -1)
+
+                    return {
+                        id: event.id,
+                        title: submission.title,
+                        startTime: event.start_time,
+                        endTime: event.end_time,
+                        place: place,
+                        track: track,
+                        speakers: eventSpeakers,
+                        experienceLevel: level,
+                        type: event.type,
+                        description: submission.abstract
+                    }
+                })
+            }
+
+            return schedulePages.add(schedulePage)
+        }))
+    }).then(() => {
+        response.status(200).send('Yay!')
+    })
+}
--- functions/src/main.ts
+++ functions/src/main.ts
@@ -2,6 +2,7 @@ import {https, firestore, config} from 'firebase-functions'
 import {initializeApp} from 'firebase-admin'
 import {migrateToFirestore} from './migrateToFirestore'
 import {hoverboardState} from './hoverboard/hoverboard'
+import { generateSchedule } from './generateSchedule';
 
 const firebaseConf = config().firebase
 const firebaseApp = initializeApp(firebaseConf)
@@ -9,4 +10,5 @@ const firebaseApp = initializeApp(firebaseConf)
 export = {
     migrateToFirestore: https.onRequest(migrateToFirestore(firebaseApp)),
     speakersToHoverboard: https.onRequest(hoverboardState(firebaseApp)),
+    generateSchedule: https.onRequest(generateSchedule(firebaseApp))
 }
