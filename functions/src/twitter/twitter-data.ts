bb364f9f7820d1102df86a08e54ee0c05506c15e
Daniele Conti <daniele.conti@novoda.com>
2018-01-19 20:38:18 +0000

    WIP twitter data

---
+++ functions/src/twitter/twitter-data.ts
@@ -0,0 +1,99 @@
+export interface SearchResult {
+    statuses: Tweet[]
+}
+
+
+export interface Tweet {
+    id_str: string
+    text: string
+    created_at: string
+    display_text_range: [number, number]
+    user: User
+    entities: {
+        hashtags: Hashtag[]
+        media: Media[]
+        urls: Url[]
+        user_mentions: UserMention[]
+    }
+}
+
+
+export interface Hashtag {
+    start: number
+    end: number
+    text: string
+}
+
+
+export interface HashtagsEntity {
+    text: string
+    [start, end]: [number, number]
+}
+
+export interface User {
+    id_str: string
+    name: string
+    screen_name: string
+    profile_image_url: string
+
+    profile_sidebar_fill_color: string
+    profile_sidebar_border_color: string
+    profile_background_tile: boolean
+    created_at: string
+    location: string
+    follow_request_sent?: null
+    profile_link_color: string
+    is_translator: boolean
+    entities: Entities1
+    default_profile: boolean
+    contributors_enabled: boolean
+    favourites_count: number
+    url?: string | null
+    profile_image_url_https: string
+    utc_offset: number
+    id: number
+    profile_use_background_image: boolean
+    listed_count: number
+    profile_text_color: string
+    lang: string
+    followers_count: number
+    protected: boolean
+    notifications?: null
+    profile_background_image_url_https: string
+    profile_background_color: string
+    verified: boolean
+    geo_enabled: boolean
+    time_zone: string
+    description: string
+    default_profile_image: boolean
+    profile_background_image_url: string
+    statuses_count: number
+    friends_count: number
+    following?: null
+    show_all_inline_media: boolean
+}
+
+export interface Entities {
+    urls?: (null)[] | null
+    hashtags?: (HashtagsEntity)[] | null
+    user_mentions?: (null)[] | null
+}
+export interface Metadata {
+    iso_language_code: string
+    result_type: string
+}
+export interface Entities1 {
+    url: Url
+    description: Description
+}
+export interface Url {
+    urls?: (UrlsEntity)[] | null
+}
+export interface UrlsEntity {
+    expanded_url?: null
+    url: string
+    indices?: (number)[] | null
+}
+export interface Description {
+    urls?: (null)[] | null
+}
