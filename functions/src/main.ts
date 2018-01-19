2c0f2c310f5070ca6f2c62d45598a03e584fe320
Daniele Conti <daniele.conti@novoda.com>
2018-01-19 18:32:52 +0000

    Hook up fetch twitter function

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -2,11 +2,14 @@ import { https, config } from 'firebase-functions'
 import { initializeApp } from 'firebase-admin'
 import { migrateToFirestore } from './migrateToFirestore'
 import { generateSchedule } from './schedule-view/generate-schedule'
+import { fetchTwitter } from './twitter/fetch-twitter';
+import fetch from "node-fetch";
 
 const firebaseConf = config().firebase
 const firebaseApp = initializeApp(firebaseConf)
 
 export = {
     migrateToFirestore: https.onRequest(migrateToFirestore(firebaseApp)),
-    generateSchedule: https.onRequest(generateSchedule(firebaseApp))
+    generateSchedule: https.onRequest(generateSchedule(firebaseApp)),
+    fetchTwitter: https.onRequest(fetchTwitter(firebaseApp, fetch, config().twitter))
 }
