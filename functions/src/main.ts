8bb822ebc414aeba7d975b5bba538c2d496796dd
Daniele Conti <daniele.conti@novoda.com>
2018-01-30 21:22:00 +0000

    Add speakers generator to functions

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -4,6 +4,7 @@ import { migrateToFirestore } from './migrateToFirestore'
 import { generateSchedule } from './schedule-view/generate-schedule'
 import { fetchTwitter } from './twitter/fetch-twitter';
 import fetch from "node-fetch";
+import { generateSpeakers } from './speakers-view/generate-speakers';
 
 const firebaseConf = config().firebase
 const firebaseApp = initializeApp(firebaseConf)
@@ -11,5 +12,6 @@ const firebaseApp = initializeApp(firebaseConf)
 export = {
     migrateToFirestore: https.onRequest(migrateToFirestore(firebaseApp)),
     generateSchedule: https.onRequest(generateSchedule(firebaseApp)),
-    fetchTwitter: https.onRequest(fetchTwitter(firebaseApp, fetch, config().twitter))
+    generateSpeakers: https.onRequest(generateSpeakers(firebaseApp)),
+    fetchTwitter: https.onRequest(fetchTwitter(firebaseApp, fetch, config().twitter)),
 }
