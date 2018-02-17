e3184578c4bd79647d7fc49207e1d21a16ce6207
Daniele Conti <daniele.conti@novoda.com>
2018-02-17 21:07:29 +0000

    Add patch to functions

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -3,6 +3,7 @@ import { config, https } from 'firebase-functions'
 import fetch from 'node-fetch'
 
 import { migrateToFirestore } from './migrateToFirestore'
+import { patch } from './patch/patch'
 import { generateSchedule } from './schedule-view/generate-schedule'
 import { generateSpeakers } from './speakers-view/generate-speakers'
 import { fetchTwitter } from './twitter/fetch-twitter'
@@ -15,4 +16,5 @@ export = {
     generateSchedule: https.onRequest(generateSchedule(firebaseApp)),
     generateSpeakers: https.onRequest(generateSpeakers(firebaseApp)),
     migrateToFirestore: https.onRequest(migrateToFirestore(firebaseApp)),
+    patch: https.onRequest(patch(firebaseApp))
 }
