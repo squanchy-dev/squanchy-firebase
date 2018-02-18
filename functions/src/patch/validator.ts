a4de597211c7f8e43c47c599ca1617ea36e7b853
Daniele Conti <daniele.conti@novoda.com>
2018-02-18 18:56:52 +0000

    Add other validators apart from required

--- functions/src/patch/validator.ts
+++ functions/src/patch/validator.ts
@@ -1,4 +1,3 @@
-
 export interface Success {
     type: 'success'
 }
@@ -8,7 +7,7 @@ export interface Failure {
 }
 
 export type Result = Success | Failure
-export type Validator = (value: any) => Result
+export type Validator = (value?: any) => Result
 
 export const success = (): Success => ({
     type: 'success'
@@ -19,4 +18,23 @@ export const failure = (message: string): Failure => ({
     type: 'failure'
 })
 
-export const required = (value: any) => value ? success() : failure('Required')
+type Predicate<T> = (value?: T) => boolean
+const validate = <T>(predicate: Predicate<T>, failureMessage: string) => (value?: T) => {
+    return (predicate(value)) ? success() : failure(failureMessage)
+}
+
+const validateOptional = <T>(predicate: Predicate<T>, failureMessage: string) => {
+    return validate<T>(it => (it === undefined || it === null) || predicate(it), failureMessage)
+}
+
+export const required = validate<any>(it => it, 'Required')
+export const isString = validateOptional<any>(it => typeof(it) === 'string', 'String')
+export const isDate = validateOptional<any>(it => it instanceof Date, 'Date')
+export const isInteger = validateOptional<any>(it => Number.isInteger(it), 'Integer')
+export const isReference = validateOptional<any>(it => {
+    return it.id && it.parent && it.path
+}, 'Reference')
+export const isArray = (validators: Validator[]) => validateOptional<any[]>(
+    list => list!.every(it => validators.every(validator => validator(it).type === 'success')),
+    'Array'
+)
