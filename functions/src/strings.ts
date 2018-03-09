ef6f8ffdc452074db64d5d719f99e80da9c2ba78
Sebastiano Poggi <poggos@gmail.com>
2018-03-09 09:27:46 +0000

    Revert back to using trim() in ensureNotEmpty()

--- functions/src/strings.ts
+++ functions/src/strings.ts
@@ -1,5 +1,5 @@
 export const ensureNotEmpty = (text: string, name: string) => {
-    if (text.replace(/\s/g, '').length < 1) {
+    if (text.trim().length < 1) {
         throw new Error(`The provided string ${name} was empty but it must be non empty`)
     }
 }
