8c73677188738c4dc219060db56d22dbce699966
Sebastiano Poggi <poggos@gmail.com>
2018-03-28 14:48:30 +0100

    Add missing export & import pair to awaitObject

--- functions/src/promise.ts
+++ functions/src/promise.ts
@@ -1,4 +1,4 @@
-const awaitObject = <T>(promisesObject: { [key: string]: Promise<T> })
+export const awaitObject = <T>(promisesObject: { [key: string]: Promise<T> })
     : Promise<{ [key: string]: T }> => {
     return Promise.all(Object.keys(promisesObject).map(key =>
         promisesObject[key].then(value => ({ key, value }))
