3abe5cb97b15407a58459f49e61d6c3996033174
Sebastiano Poggi <poggos@gmail.com>
2018-10-23 10:17:11 +0200

    Cleanup http-trigger code

--- functions/src/http/config.ts
+++ functions/src/http/config.ts
@@ -1,5 +1,1 @@
 export interface HttpConfig { print_error_details: boolean }
-
-export const shouldPrintErrorDetails = ({ print_error_details }: HttpConfig) => {
-    return print_error_details
-}
--- functions/src/http/http-trigger.ts
+++ functions/src/http/http-trigger.ts
@@ -1,26 +1,29 @@
 import { Request, Response } from 'express'
-import { shouldPrintErrorDetails, HttpConfig } from './config'
+import { HttpConfig } from './config'
 
 export const httpTrigger = (callback: () => Promise<any>, config: HttpConfig) => {
-    return httpTriggerInternal(callback, shouldPrintErrorDetails(config))
+    return httpTriggerInternal(callback, errorMessageFor(config))
 }
 
-const httpTriggerInternal = (callback: () => Promise<any>, printErrorDetails: boolean) =>
+const httpTriggerInternal = (callback: () => Promise<any>, errorMessageProvider: (error: Error) => boolean) =>
     async (_: Request, response: Response) => {
         try {
             await callback()
             response.status(200).send('<h1>🚀 Success</h1>')
         } catch (error) {
             console.error(error)
-            let errorMessage: string
-            if (printErrorDetails) {
-                errorMessage = error.stack
-            } else {
-                errorMessage = error
-            }
+            const errorMessage = errorMessageProvider(error)
             response.status(500)
                 .send(`<h1>❌ Ooops...</h1><p>Something went wrong:</p>` +
                     `<pre width=75% style="margin: 32px; background-color: #eff0f1; padding: 16px; overflow: auto;">` +
                     `${errorMessage}</pre>`)
         }
     }
+
+const errorMessageFor = ({ print_error_details }: HttpConfig) => (error: any) => {
+    if (print_error_details) {
+        return error.stack
+    } else {
+        return error
+    }
+}
